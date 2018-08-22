186a08f02929b59868b78e2c583104c01fdac83c
MaJunChaoGit <354488954@qq.com>
2018-08-22 16:11:57 +0800

    调整下服务器路径

--- examples/api/index.js
+++ examples/api/index.js
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development';
 
 export default {
-  newYork: isDev ? 'http://192.168.1.21:8888/NewYork/tileset.json' : 'http://static.mozhengying.net/NewYork/tileset.json'
+  newYork: isDev ? 'http://192.168.1.21:8888/NewYork/tileset.json' : 'http://mozhengying.com:32768/NewYork/tileset.json'
 };
