e3c52c3ba9a570a77996843c763d4542642f9757
MaJunChaoGit <354488954@qq.com>
2018-08-22 15:30:12 +0800

    试一下版本

--- examples/api/index.js
+++ examples/api/index.js
@@ -1,3 +1,5 @@
+const isDev = process.env.NODE_ENV === 'development';
+
 export default {
-  newYork: 'http://192.168.1.21:8888/NewYork/tileset.json'
+  newYork: isDev ? 'http://192.168.1.21:8888/NewYork/tileset.json' : 'http://static.mozhengying.net/NewYork/tileset.json'
 };
