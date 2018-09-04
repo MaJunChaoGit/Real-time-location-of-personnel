d3e0ad550d1bf77a791d810902805d4268a31990
MaJunChaoGit <354488954@qq.com>
2018-09-04 11:44:53 +0800

    写了个button-group和button组件，调好样式了，下午合功能

---
+++ examples/components/functionality/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button
+    class="rp-button"
+    @click="handleClick"
+    :type="nativeType"
+    :class="[
+      { 'is-round': round }
+    ]"
+  >
+    <i class="rp-icon-loading" v-if="loading"></i>
+    <i :class="icon" v-if="icon && !loading"></i>
+    <span v-if="$slots.default"><slot></slot></span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'RpButton',
+
+  props: {
+    nativeType: {
+      type: String,
+      default: 'button'
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    loading: Boolean,
+    round: Boolean
+  },
+
+  methods: {
+    handleClick(evt) {
+      this.$emit('click', evt);
+    }
+  }
+};
+</script>
--- examples/components/layout/RightSideBar.vue
+++ examples/components/layout/RightSideBar.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="rp-right-side">
-    <div class="rp-right-side__navigation" id="rp-outring"></div>
+    <div class="rp-right-side__item rp-right-side__navigation" id="rp-outring"></div>
+    <div class="rp-right-side__item">
+      <rp-button-group direction="column">
+        <rp-button round>a</rp-button>
+        <rp-button round>b</rp-button>
+      </rp-button-group>
+    </div>
   </div>
 </template>
 <script>
-
+import RpButtonGroup from './ButtonGroup';
+import RpButton from '../functionality/Button';
 export default {
-  name: 'RpRightSide'
+  name: 'RpRightSide',
+
+  components: {
+    RpButtonGroup,
+    RpButton
+  }
 };
 </script>
